def7d2c30b496ec6e4f110ef10f0d84bb1bc4477
2Archik <artur.avitolog@yandex.ru>
2023-02-05 13:10:38 +0500

    miniProject-COLORS

---
+++ app.js
@@ -0,0 +1,94 @@
+const cols = document.querySelectorAll(".col");
+
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  if (event.code.toLowerCase() === "space") {
+    setRandomColors();
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === "Lock") {
+    const node =
+      event.target.tagName.toLowerCase() === "i"
+        ? event.target
+        : event.target.children[0];
+    node.classList.toggle("fa-lock-open");
+    node.classList.toggle("fa-lock");
+  } else if (type === "Copy") {
+    copyToClickBoard(event.target.textContent);
+  }
+});
+
+function generateRandomColor() {
+  const hexCodes = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return "#" + color;
+}
+
+function copyToClickBoard(text) {
+  return navigator.clipboard.writeText(text);
+}
+
+function setRandomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : [];
+
+  cols.forEach((col, index) => {
+    const isLocked = col.querySelector("i").classList.contains("fa-lock");
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
+
+    if (isLocked) {
+      colors.push(text.textContent);
+      return;
+    }
+
+    const color = isInitial
+      ? colors[index]
+        ? colors[index]
+        : generateRandomColor()
+      : generateRandomColor();
+
+    if (!isInitial) {
+      colors.push(color);
+    }
+
+    text.textContent = color;
+    col.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
+
+  updateColorsHash(colors);
+}
+
+function setTextColor(text, color) {
+  const Luminance = chroma(color).luminance();
+  text.style.color = Luminance > 0.5 ? "black" : "white";
+}
+
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1);
+    })
+    .join("-");
+}
+
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split("-")
+      .map((color) => "#" + color);
+  }
+  return [];
+}
+
+setRandomColors(true);
